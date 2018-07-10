54ee8020aa45d624df01f46af35c18c615c546f6
Uthaeus <romanlavery@gmail.com>
2018-07-09 18:02:21 -0600

    Created views for chats and changed root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'sessions#new'
+  root 'chats#index'
 
   resources :users, only: [:new, :create] do 
     resources :chats, only: [:index, :show, :create]
